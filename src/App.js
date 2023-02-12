5fceeb1527e131f039790a74172ac7baaf5a291d
theapratap <theapratap@gmail.com>
2023-02-12 16:50:11 +0530

    Created seperate overview

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import IncomeTax from './IncomeTax';
 import TaxBreakdown from './TaxBreakdown';
+import Overview from './Overview';
 
 const App = () => {
   return (
@@ -10,8 +11,7 @@ const App = () => {
           <h2>Income Tax Calculator, India</h2><h5>Estimator for Taxes, According to Union Budget 2023-2024</h5>
       <IncomeTax />
       <TaxBreakdown/>
-      <h2>Income Tax Department, Ministry of Finance, India</h2>
-        <p>The Income Tax Department, under the Ministry of Finance in India, is responsible for enforcing laws and regulations related to the assessment and collection of taxes on income. The department works to ensure fair and efficient tax collection while providing support and assistance to taxpayers.</p>
+      <Overview/>
     </div>
   );
 }
---
+++ src/Overview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Overview = () => {
+    return (
+        <div>
+            <h2>Income Tax Department, Ministry of Finance, India</h2>
+            <p>The Income Tax Department, under the Ministry of Finance in India, is responsible for enforcing laws and regulations related to the assessment and collection of taxes on income. The department works to ensure fair and efficient tax collection while providing support and assistance to taxpayers.</p>
+        </div>
+    )
+};
+
+export default Overview;
